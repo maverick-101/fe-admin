fa9f89fdbdcfdd906796082565ae86a2b359c2ac
arslansajid <arslansajid951@gmail.com>
2019-02-07 23:14:43 +0500

    fixed

--- client/containers/HotelForm.jsx
+++ client/containers/HotelForm.jsx
@@ -52,7 +52,7 @@ export default class HotelForm extends React.Component {
   }
 
   componentWillMount() {
-    axios.get(`${this.endPoint}/api/city/fetch`)
+    axios.get(`${this.endPoint}/api/fetch/city-fetch`)
         .then((response) => {
           this.setState({
             cities: response.data,
